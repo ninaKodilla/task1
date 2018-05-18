7fbc337917102a3ee975f21a3fa7958a87b77acd
ninaKodilla <nkozlowska89@gmail.com>
2018-05-18 10:24:36 +0200

    small change

--- program.js
+++ program.js
@@ -3,7 +3,7 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
 
-    (!input) ? null : switched(input) ;
+    (!input) ? null : switched(input);
 });
 
 function switched(input) {
