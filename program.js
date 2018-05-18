f6c34b5a6c6e01d5ffd40533623255cde11d4f80
ninaKodilla <nkozlowska89@gmail.com>
2018-05-18 10:23:54 +0200

    short if

--- program.js
+++ program.js
@@ -2,10 +2,8 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    
-    if (input !== null) {
-        switched(input);
-    }
+
+    (!input) ? null : switched(input) ;
 });
 
 function switched(input) {
