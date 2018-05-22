417623d9df3dab8ad64868ac11a4526c0d5c2734
npupiec <nkozlowska89@gmail.com>
2018-05-22 17:04:03 +0200

    fixes code

--- program.js
+++ program.js
@@ -3,7 +3,9 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
 
-    (!input) ? null : switched(input);
+    if (input) {
+        switched(input);
+    }
 });
 
 function switched(input) {
